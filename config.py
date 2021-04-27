2ec66b9c98f237f65291f82fe2df32b39be5b45b
jhong <x29420953@gmail.com>
2021-04-27 23:09:43 +0800

    second deploy

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_SECRET_KEY = "My Key"
     SECRET_KEY = "Your Key"
+    SQLALCHEMY_POOL_RECYCLE = 90
     #mysql://b1ad405ad73ea5:25c66eb7@us-cdbr-east-03.cleardb.com/heroku_637d5fce379a2f7?reconnect=true
     #mysql+pymysql://root:88888888@127.0.0.1:3307/shop
