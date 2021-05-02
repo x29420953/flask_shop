4d5b1b1debfbea1c071891d9f70d20ee71105bd7
jhong <x29420953@gmail.com>
2021-05-02 17:27:39 +0800

    css adjust

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:88888888@127.0.0.1:3307/shop"
+    SQLALCHEMY_DATABASE_URI = "mysql://b1ad405ad73ea5:25c66eb7@us-cdbr-east-03.cleardb.com/heroku_637d5fce379a2f7"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_SECRET_KEY = "My Key"
     SECRET_KEY = "Your Key"
